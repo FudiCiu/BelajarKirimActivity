2085aee72a9ca6e300813d415f95c6751598382d
FudiCiu <fudiantociuandi_17@kharisma.ac.id>
2019-04-25 12:19:55 +0800

    Initial commit

---
+++ app/src/main/java/com/latihanandroid/belajarkirimactivity/MainActivity.java
@@ -0,0 +1,44 @@
+package com.latihanandroid.belajarkirimactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText etData1= (EditText) findViewById(R.id.et_data1);
+        final EditText etData2= (EditText) findViewById(R.id.et_data2);
+        Button btSubmit= (Button) findViewById(R.id.bt_submit);
+        Button btSubmitIntent= (Button) findViewById(R.id.bt_submitintent);
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle= new Bundle();
+                bundle.putString("data1",etData1.getText().toString());
+                bundle.putString("data2",etData2.getText().toString());
+                Intent intent= new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+        btSubmitIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("data1",etData1.getText().toString());
+                intent.putExtra("data2",etData2.getText().toString());
+
+                startActivity(intent);
+            }
+        });
+    }
+
+}
